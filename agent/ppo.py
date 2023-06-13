bde787c22920f2b6fb1e703a16ce2e58a8b7e0e2
SohyunNam <sohyon525@naver.com>
2023-06-13 10:08:01 +0900

    add path of adahessian

--- agent/ppo.py
+++ agent/ppo.py
@@ -3,7 +3,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from ada_hessian import AdaHessian
+from agent.ada_hessian import AdaHessian
 
 torch.manual_seed(42)
 
